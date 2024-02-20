6c63ca77115595b80a49f27665194c54c97c59ae
emily shea <emilyshea947@gmail.com>
2024-02-20 13:40:54 -0500

    tag and product tag

--- models/Product.js
+++ models/Product.js
@@ -23,13 +23,26 @@ Product.init(
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
       validate: {
-        isDecimal: true
+        isDecimal: true,
       }
-     
-
-    }
-    
+    },
+     stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    },
   },
+    
   {
     sequelize,
     timestamps: false,
